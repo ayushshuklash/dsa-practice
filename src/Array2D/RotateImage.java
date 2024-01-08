373cafc72bd94de0928595398c48759c835dfa73
Ayush Shukla <ayush.shuklash@gmail.com>
2024-01-08 20:32:17 +0530

    DSA-DAY4 RotateImage

---
+++ src/Array2D/RotateImage.java
@@ -0,0 +1,54 @@
+package Array2D;
+
+import java.util.Scanner;
+
+public class RotateImage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            System.out.println("Enter length");
+            int n = sc.nextInt();
+            int[][] arr = new int[n][];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            new RotateImage().rotate(arr);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j]);
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
+    /*
+    Solution :- 1.Make head  -> toe  and toe -> head
+                2. Make Left -> right and right -> left
+     */
+    public void rotate(int[][] matrix) {
+        int s = 0, e = matrix.length - 1;
+        while (s < e) {
+            int[] temp = matrix[s];
+            matrix[s] = matrix[e];
+            matrix[e] = temp;
+            s++;
+            e--;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+}
